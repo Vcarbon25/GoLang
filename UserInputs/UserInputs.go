c8bc2ffece0b5fbb7c3dd7f4e549e7ef18dcdc82
Victor Carbón Ferreira <92864869+Vcarbon25@users.noreply.github.com>
2026-09-03 15:00:48 -0300

    Enhance input handling and output formatting
    Added input handling for string without spaces and updated output formatting.

--- UserInputs/UserInputs.go
+++ UserInputs/UserInputs.go
@@ -15,6 +15,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("The input was: %s", input)
+
+	fmt.Printf("The input was: %s\n", input)
+	fmt.Println("Insira string sem espaço ")
+	var dado string
+	//in fmt scan each word separated by space " " will be put in a diferrent variable, bellow only accepts 1
+	fmt.Scanln(&dado)
+	fmt.Printf("Fmt scan got: %s", dado)
 
 }
